aa2ba28ffcd5c6f6017f58c1d452d239bfecb78f
nataly <19nat.9295@gmail.com>
2025-04-28 22:34:56 -0500

    Subo ejercicio de modulos correspondiente al modulo V

---
+++ modulos.py/emoji.py
@@ -0,0 +1,2 @@
+import emoji
+nota = float(input(emoji.emojize("cuál fue tu nota :hear_no_evil_monkey: ?")))
---
+++ modulos.py/modulos.py
@@ -0,0 +1,6 @@
+from matematicas import *
+
+print(suma(3,4))
+print(resta(3,4))
+print(multiplicacion(3,4))
+print(division(3,4))
